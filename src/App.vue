bd4f3f6b27497bd872de33b8a97ccb6b942bf159
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-01-18 15:49:22 +0100

    update

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import AppHeader from './components/Header/AppHeader.vue'
+import AppMain from './components/Main/AppMain.vue'
+import AppFooter from './components/Footer/AppFooter.vue'
+
+export default {
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+  data(){
+    return {
+      
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <AppHeader />
+
+  <AppMain />
+
+  <AppFooter />
+  
+</template>
+
+<style lang="scss">
+@use './bootstrap/scss/bootstrap.scss' as *; 
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/variables' as *;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap');
+
+*{
+  font-family: 'Roboto', sans-serif;
+}
+
+</style>
---
+++ src/components/Header/AppHeader.vue
@@ -0,0 +1,40 @@
+<script>
+
+export default {
+    methods: {
+        getImagePath: function (imgPath){
+            return new URL(`../../assets/img/${imgPath}`, import.meta.url).href;
+        }
+    }
+    
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row d-flex justify-content-center justify-content-between align-items-center">
+            <div class="col-2">
+                <img :src="getImagePath('favicon-gobike.png')" alt="logo">
+            </div>
+            <div id="header-menu" class="col-4">
+                menu
+            </div>
+            <div id="upcoming-events" class="col-3">
+                events
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<style lang="scss" scoped>
+
+    #header-menu{
+        background-color: green;
+    }
+
+    #upcoming-events{
+        background-color: blue;
+    }
+    
+</style>
